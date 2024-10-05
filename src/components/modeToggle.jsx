439f6f433262b6752c025a2271a8b75399c44b5a
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-10-05 05:37:09 +0300

    redux store and dark mode

---
+++ src/components/modeToggle.jsx
@@ -0,0 +1,29 @@
+import { Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode } from "@/slices/modeSlice";
+
+function ModeToggle() {
+  const { mode } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <Button
+        variant="outline"
+        size="icon"
+        onClick={() => {
+          mode.theme === "light"
+            ? dispatch(setMode("dark"))
+            : dispatch(setMode("light"));
+        }}
+      >
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100" />
+        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-0 transition-all dark:rotate-90 dark:scale-0" />
+        <span className="sr-only">Toggle theme</span>
+      </Button>
+    </>
+  );
+}
+
+export default ModeToggle;
