6b501b5ec2ac7944670875b2ab3e975a67227ea9
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-10-05 07:42:38 +0300

    pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/shared/Navbar";
+import Home from "./pages/Home";
+import Categories from "./pages/Categories";
+import Products from "./pages/Products";
+import Orders from "./pages/Orders";
+import Customers from "./pages/Customers";
+import Reports from "./pages/Reports";
+import Promotions from "./pages/Promotions";
+import Settings from "./pages/Settings";
+import Owner from "./pages/Owner";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/customers" element={<Customers />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="/promotions" element={<Promotions />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/owner" element={<Owner />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import store from "./store/store.js";
 import App from "./App.jsx";
 
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </StrictMode>
 );
