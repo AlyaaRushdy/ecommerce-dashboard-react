d3e8f0dfa39c1caf25f79fc0052a05fe04bd0048
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-10-05 23:32:31 +0300

    app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/shared/Navbar";
+import Home from "./pages/Home";
+import Categories from "./pages/Categories";
+import Products from "./pages/Products";
+import Orders from "./pages/Orders";
+import Customers from "./pages/Customers";
+import Reports from "./pages/Reports";
+import Promotions from "./pages/Promotions";
+import Settings from "./pages/Settings";
+import Owner from "./pages/Owner";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <main className="lg:w-[calc(100%-112px)] w-full ml-auto pl-2">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/promotions" element={<Promotions />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/owner" element={<Owner />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
