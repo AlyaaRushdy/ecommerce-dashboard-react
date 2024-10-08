319d33f96962b949386a34cc74dba59939eb28e8
MostafaMagdy21 <mostafamagdy2196@gmail.com>
2024-10-09 01:03:40 +0300

    add products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,267 @@
-function Products() {
-  return (
-    <>
-      <h1>Products Page</h1>
-    </>
-  );
-}
+"use client";
 
-export default Products;
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
+import {
+	Card,
+	CardContent,
+	CardHeader,
+	CardTitle,
+	CardDescription,
+} from "@/components/ui/card";
+import {
+	ChartContainer,
+	ChartTooltip,
+	ChartTooltipContent,
+} from "@/components/ui/chart";
+import Header from "../components/Header";
+import { BellRing } from "lucide-react";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { CiFilter } from "react-icons/ci";
+import { FaEdit } from "react-icons/fa";
+import { RiDeleteBin6Fill } from "react-icons/ri";
+import { ImFolderOpen } from "react-icons/im";
+import { MdOutlineCreateNewFolder } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+export default function CardWithForm() {
+	const notifications = [
+		{
+			title: "Your call has been confirmed.",
+			description: "1 hour ago",
+		},
+		{
+			title: "You have a new message!",
+			description: "1 hour ago",
+		},
+		{
+			title: "Your subscription is expiring soon!",
+			description: "2 hours ago",
+		},
+	];
+
+	const chartData = [
+		{ month: "January", desktop: 186 },
+		{ month: "February", desktop: 305 },
+		{ month: "March", desktop: 237 },
+		{ month: "April", desktop: 73 },
+		{ month: "May", desktop: 209 },
+		{ month: "June", desktop: 214 },
+	];
+
+	const chartConfig = {
+		desktop: {
+			label: "Desktop",
+			color: "hsl(var(--chart-1))",
+		},
+	};
+	return (
+		<main>
+			<section className="header">
+				<Header />
+			</section>
+			{/* start card */}
+			<section className="row m-5 grid grid-cols-12 gap-4">
+				<div className="card md:col-span-4 lg:col-span-4 col-span-12 ">
+					<Card>
+						<CardHeader>
+							<CardTitle>Notifications</CardTitle>
+							<CardDescription>You have 3 unread messages.</CardDescription>
+						</CardHeader>
+						<CardContent className="grid gap-4">
+							<div className=" flex items-center space-x-4 rounded-md border p-4">
+								<BellRing />
+								<div className="flex-1 space-y-1">
+									<p className="text-sm font-medium leading-none">
+										Push Notifications
+									</p>
+									<p className="text-sm text-muted-foreground">
+										Send notifications to device.
+									</p>
+								</div>
+							</div>
+							<div>
+								{notifications.map((notification, index) => (
+									<div
+										key={index}
+										className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+									>
+										<span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
+										<div className="space-y-1">
+											<p className="text-sm font-medium leading-none">
+												{notification.title}
+											</p>
+											<p className="text-sm text-muted-foreground">
+												{notification.description}
+											</p>
+										</div>
+									</div>
+								))}
+							</div>
+						</CardContent>
+					</Card>
+				</div>
+				{/* start chart */}
+				<section className="chart md:col-span-8 lg:col-span-8 col-span-12">
+					<Card>
+						<CardHeader>
+							<CardTitle>Line Chart</CardTitle>
+						</CardHeader>
+						<CardContent>
+							<ChartContainer config={chartConfig}>
+								<LineChart
+									accessibilityLayer
+									data={chartData}
+									margin={{
+										left: 12,
+										right: 12,
+									}}
+								>
+									<CartesianGrid vertical={false} />
+									<XAxis
+										dataKey="month"
+										tickLine={false}
+										axisLine={false}
+										tickMargin={8}
+										tickFormatter={(value) => value.slice(0, 3)}
+									/>
+									<ChartTooltip
+										cursor={false}
+										content={<ChartTooltipContent hideLabel />}
+									/>
+									<Line
+										dataKey="desktop"
+										type="natural"
+										stroke="var(--color-desktop)"
+										strokeWidth={2}
+										dot={false}
+									/>
+								</LineChart>
+							</ChartContainer>
+						</CardContent>
+					</Card>
+				</section>
+			</section>
+			{/* start table */}
+			<section className="row flex justify-end m-5">
+				<div className="mx-1">
+					<Button variant="outline">
+						Create <MdOutlineCreateNewFolder />
+					</Button>
+				</div>
+				<div className="mx-1">
+					<Button variant="outline">
+						Filter By <CiFilter />
+					</Button>
+				</div>
+				<div className="mx-1">
+					<Button variant="outline">
+						Sort By <CiFilter />
+					</Button>
+				</div>
+			</section>
+			<section className="table m-5 grid grid-cols-1">
+				<Card className="w-full">
+					<Table>
+						<TableHeader className="bg-gray-200">
+							<TableRow>
+								<TableHead>Code</TableHead>
+								<TableHead>Title</TableHead>
+								<TableHead>Price</TableHead>
+								<TableHead>Quentity</TableHead>
+								<TableHead>Date</TableHead>
+								<TableHead>Status</TableHead>
+								<TableHead>Actions</TableHead>
+							</TableRow>
+						</TableHeader>
+						<TableBody>
+							<TableRow>
+								<TableCell>INV001</TableCell>
+								<TableCell>Paid</TableCell>
+								<TableCell>$250.00</TableCell>
+								<TableCell>Credit Card</TableCell>
+								<TableCell>INV001</TableCell>
+								<TableCell>Paid</TableCell>
+								<TableCell className="grid grid-cols-3">
+									<Link to="#">
+										<ImFolderOpen />
+									</Link>
+									<Link to="#">
+										<FaEdit />
+									</Link>
+									<Link to="#">
+										<RiDeleteBin6Fill />
+									</Link>
+								</TableCell>
+							</TableRow>
+							<TableRow>
+								<TableCell>INV001</TableCell>
+								<TableCell>Paid</TableCell>
+								<TableCell>$250.00</TableCell>
+								<TableCell>Credit Card</TableCell>
+								<TableCell>INV001</TableCell>
+								<TableCell>Paid</TableCell>
+								<TableCell className="grid grid-cols-3">
+									<Link to="#">
+										<ImFolderOpen />
+									</Link>
+									<Link to="#">
+										<FaEdit />
+									</Link>
+									<Link to="#">
+										<RiDeleteBin6Fill />
+									</Link>
+								</TableCell>
+							</TableRow>
+							<TableRow>
+								<TableCell>INV001</TableCell>
+								<TableCell>Paid</TableCell>
+								<TableCell>$250.00</TableCell>
+								<TableCell>Credit Card</TableCell>
+								<TableCell>INV001</TableCell>
+								<TableCell>Paid</TableCell>
+								<TableCell className="grid grid-cols-3">
+									<Link to="#">
+										<ImFolderOpen />
+									</Link>
+									<Link to="#">
+										<FaEdit />
+									</Link>
+									<Link to="#">
+										<RiDeleteBin6Fill />
+									</Link>
+								</TableCell>
+							</TableRow>
+							<TableRow>
+								<TableCell>INV001</TableCell>
+								<TableCell>Paid</TableCell>
+								<TableCell>$250.00</TableCell>
+								<TableCell>Credit Card</TableCell>
+								<TableCell>INV001</TableCell>
+								<TableCell>Paid</TableCell>
+								<TableCell className="grid grid-cols-3">
+									<Link to="#">
+										<ImFolderOpen />
+									</Link>
+									<Link to="#">
+										<FaEdit />
+									</Link>
+									<Link to="#">
+										<RiDeleteBin6Fill />
+									</Link>
+								</TableCell>
+							</TableRow>
+						</TableBody>
+					</Table>
+				</Card>
+			</section>
+		</main>
+	);
+}
