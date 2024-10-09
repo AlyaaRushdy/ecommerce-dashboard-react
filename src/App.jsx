04100775d8747eb640a6a84748fa0fe52b09626f
MostafaMagdy21 <mostafamagdy2196@gmail.com>
2024-10-10 01:21:58 +0300

    add single product page

--- src/App.jsx
+++ src/App.jsx
@@ -10,27 +10,29 @@ import Settings from "./pages/Settings";
 import Sidebar from "./components/shared/Sidebar";
 import CustomerDetails from "./pages/CustomerDetails";
 import ErrorPage from "./pages/ErrorPage";
+import SingleProduct from "./pages/SingleProduct";
 
 function App() {
-  return (
-    <>
-      <Sidebar />
-      <main className="w-full sm:w-[calc(100%-6rem)] md:w-[calc(100%-7rem)] ml-auto bg-gray-100 dark:bg-background min-h-screen">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/customers" element={<Customers />} />
-          <Route path="/customers/:customerId" element={<CustomerDetails />} />
-          <Route path="/reports" element={<Reports />} />
-          <Route path="/promotions" element={<Promotions />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="*" element={<ErrorPage/>} />
-        </Routes>
-      </main>
-    </>
-  );
+	return (
+		<>
+			<Sidebar />
+			<main className="w-full sm:w-[calc(100%-6rem)] md:w-[calc(100%-7rem)] ml-auto bg-gray-100 dark:bg-background min-h-screen">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/products" element={<Products />} />
+					<Route path="/products/:id" element={<SingleProduct />} />
+					<Route path="/categories" element={<Categories />} />
+					<Route path="/orders" element={<Orders />} />
+					<Route path="/customers" element={<Customers />} />
+					<Route path="/customers/:customerId" element={<CustomerDetails />} />
+					<Route path="/reports" element={<Reports />} />
+					<Route path="/promotions" element={<Promotions />} />
+					<Route path="/settings" element={<Settings />} />
+					<Route path="*" element={<ErrorPage />} />
+				</Routes>
+			</main>
+		</>
+	);
 }
 
 export default App;
