aaef75e2a987cb873bdff8a6f8e2a7c4018a2cf2
Anas Moner <ansmoner2000@gmail.com>
2024-10-17 14:46:50 +0300

    some updates in add AddCategory page

--- src/pages/AddCategory.jsx
+++ src/pages/AddCategory.jsx
@@ -1,8 +1,10 @@
+import { FileUpload } from '@/components/ui/file-upload'
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 
 const AddCategory = () => {
   return (
-    <div>AddCategory</div>
+    <FileUpload/>
   )
 }
 
