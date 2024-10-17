24b38fe6c14e01007caad3fb127080c508e4f249
AlyaaRushdy <98500365+AlyaaRushdy@users.noreply.github.com>
2024-10-17 03:59:14 +0300

    order datails

--- src/App.jsx
+++ src/App.jsx
@@ -12,29 +12,31 @@ import CustomerDetails from "./pages/CustomerDetails";
 import ErrorPage from "./pages/ErrorPage";
 import SingleProduct from "./pages/SingleProduct";
 import AddCategory from "./pages/AddCategory";
+import OrderDetails from "./pages/OrderDetails";
 
 function App() {
-	return (
-		<>
-			<Sidebar />
-			<main className="w-full sm:w-[calc(100%-6rem)] md:w-[calc(100%-7rem)] ml-auto bg-gray-100 dark:bg-background min-h-screen">
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/products" element={<Products />} />
-					<Route path="/products/:id" element={<SingleProduct />} />
-					<Route path="/categories" element={<Categories />} />
-					<Route path="/categories/create" element={<AddCategory />} />
-					<Route path="/orders" element={<Orders />} />
-					<Route path="/customers" element={<Customers />} />
-					<Route path="/customers/:customerId" element={<CustomerDetails />} />
-					<Route path="/reports" element={<Reports />} />
-					<Route path="/promotions" element={<Promotions />} />
-					<Route path="/settings" element={<Settings />} />
-					<Route path="*" element={<ErrorPage />} />
-				</Routes>
-			</main>
-		</>
-	);
+  return (
+    <>
+      <Sidebar />
+      <main className="w-full sm:w-[calc(100%-6rem)] md:w-[calc(100%-7rem)] ml-auto bg-gray-100 dark:bg-background min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<SingleProduct />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/categories/create" element={<AddCategory />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/orders/:id" element={<OrderDetails />} />
+          <Route path="/customers" element={<Customers />} />
+          <Route path="/customers/:customerId" element={<CustomerDetails />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/promotions" element={<Promotions />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
