463226757268bfd10d3573057faa0160440897c9
MennaAllahYasser11 <mennaallahyasser111@gmail.com>
2024-10-18 23:43:20 +0300

    handle function for images

--- src/pages/OrderDetails.jsx
+++ src/pages/OrderDetails.jsx
@@ -20,6 +20,14 @@ function OrderDetails() {
             });
     }, [id]);
 
+    const fixImagePath = (imagePath) => {
+
+        return imagePath.replace(
+            "E:\\DEPI\\FinalProject backend\\ecommerce-dashboard-backend\\images\\", 
+            "http://localhost:5000/images/"
+        );
+    };
+
     if (!orderDetails) {
         return <div className="text-center">Loading...</div>;
     }
@@ -31,24 +39,23 @@ function OrderDetails() {
 
                 <div className="py-4 gap-5">
                     {orderDetails.products.map((product) => (
-                        <Card key={product._id} className="flex flex-col mb-4">
+                        <Card key={product.productId} className="flex flex-col mb-4">
                             <CardContent className="flex flex-col items-center p-4">
                                 <img
-                                    src={product.images[0]}  
-                                    alt={product.title} 
-                                    className="mb-2 w-full h-auto max-w-[200px] md:max-w-[250px]" 
+                                    src={fixImagePath(product.images)}
+                                    alt={product.title}
+                                    className="mb-2 w-full h-auto max-w-[200px] md:max-w-[250px]"
                                 />
                                 <div className="text-center">
                                     <h2 className="text-lg font-bold">{product.title}</h2>
                                     <p className="text-sm mb-1"><strong className="text-orange-700 text-base">SKU : </strong> {product.sku}</p>
-                                    <p className="text-sm mb-1"><strong className="text-orange-700 text-base">Size :</strong> {product.size}</p>
                                     <p className="text-sm mb-1"><strong className="text-orange-700 text-base">Quantity :</strong> {product.quantity}</p>
-                                    <p className="text-sm mb-1"><strong className="text-orange-700 text-base">Total Price : </strong>{orderDetails.totalPrice ? orderDetails.totalPrice :  'NAN'}</p>
+                                    <p className="text-sm mb-1"><strong className="text-orange-700 text-base">Total Price : </strong>{orderDetails.totalPrice || 'NAN'}</p>
                                     <p className="text-sm mb-1"><strong className="text-orange-700 text-base">Order Date :</strong> {new Date(orderDetails.createdAt).toLocaleDateString()}</p>
                                     <p className="text-sm mb-1"><strong className="text-orange-700 text-base">Status :</strong> {orderDetails.status}</p>
-
                                 </div>
                             </CardContent>
+
                             <CardFooter className="flex justify-center">
                                 <p className="text-sm"><strong className="text-orange-700 text-base">Order ID :</strong> {orderDetails.id}</p>
                             </CardFooter>
@@ -61,5 +68,3 @@ function OrderDetails() {
 }
 
 export default OrderDetails;
-
-
