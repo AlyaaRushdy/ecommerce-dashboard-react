99eaaa60efc70ceda3c05bcb80b5ae0c2de0e50a
MennaAllahYasser11 <mennaallahyasser111@gmail.com>
2024-10-18 08:44:36 +0300

    add order details

--- src/pages/OrderDetails.jsx
+++ src/pages/OrderDetails.jsx
@@ -1,126 +1,66 @@
-import { Bar, BarChart, LabelList, XAxis, YAxis } from "recharts";
+
+
+import { useEffect, useState } from "react";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { ChartContainer } from "@/components/ui/chart";
-import { Separator } from "@/components/ui/separator";
 import Header from "@/components/shared/Header";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function OrderDetails() {
-  const { id } = useParams();
-  return (
-    <>
-      <div className="p-5">
-        <Header currentPage={id} prevPage={"Orders"} prevPageLink={"/orders"} />
-        <div className="py-4">
-          <Card className="max-w-xs">
-            <CardContent className="flex gap-4 p-4 pb-2">
-              <ChartContainer
-                config={{
-                  move: {
-                    label: "Move",
-                    color: "hsl(var(--chart-1))",
-                  },
-                  stand: {
-                    label: "Stand",
-                    color: "hsl(var(--chart-2))",
-                  },
-                  exercise: {
-                    label: "Exercise",
-                    color: "hsl(var(--chart-3))",
-                  },
-                }}
-                className="h-[140px] w-full"
-              >
-                <BarChart
-                  margin={{
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    bottom: 10,
-                  }}
-                  data={[
-                    {
-                      activity: "stand",
-                      value: (8 / 12) * 100,
-                      label: "8/12 hr",
-                      fill: "var(--color-stand)",
-                    },
-                    {
-                      activity: "exercise",
-                      value: (46 / 60) * 100,
-                      label: "46/60 min",
-                      fill: "var(--color-exercise)",
-                    },
-                    {
-                      activity: "move",
-                      value: (245 / 360) * 100,
-                      label: "245/360 kcal",
-                      fill: "var(--color-move)",
-                    },
-                  ]}
-                  layout="vertical"
-                  barSize={32}
-                  barGap={2}
-                >
-                  <XAxis type="number" dataKey="value" hide />
-                  <YAxis
-                    dataKey="activity"
-                    type="category"
-                    tickLine={false}
-                    tickMargin={4}
-                    axisLine={false}
-                    className="capitalize"
-                  />
-                  <Bar dataKey="value" radius={5}>
-                    <LabelList
-                      position="insideLeft"
-                      dataKey="label"
-                      fill="white"
-                      offset={8}
-                      fontSize={12}
-                    />
-                  </Bar>
-                </BarChart>
-              </ChartContainer>
-            </CardContent>
-            <CardFooter className="flex flex-row border-t p-4">
-              <div className="flex w-full items-center gap-2">
-                <div className="grid flex-1 auto-rows-min gap-0.5">
-                  <div className="text-xs text-muted-foreground">Move</div>
-                  <div className="flex items-baseline gap-1 text-2xl font-bold tabular-nums leading-none">
-                    562
-                    <span className="text-sm font-normal text-muted-foreground">
-                      kcal
-                    </span>
-                  </div>
+    const { id } = useParams();
+    const [orderDetails, setOrderDetails] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:5000/orders/${id}`)
+            .then((res) => {
+                setOrderDetails(res.data.results);
+            })
+            .catch((error) => {
+                console.error("Error fetching order details:", error);
+            });
+    }, [id]);
+
+    if (!orderDetails) {
+        return <div className="text-center">Loading...</div>;
+    }
+
+    return (
+        <>
+            <div className="p-5">
+                <Header currentPage={` ${id}`} prevPage={"Orders"} prevPageLink={"/orders"} />
+
+                <div className="py-4 gap-5">
+                    {orderDetails.products.map((product) => (
+                        <Card key={product._id} className="flex flex-col mb-4">
+                            <CardContent className="flex flex-col items-center p-4">
+                                <img
+                                    src={product.images[0]}  
+                                    alt={product.title} 
+                                    className="mb-2 w-full h-auto max-w-[200px] md:max-w-[250px]" 
+                                />
+                                <div className="text-center">
+                                    <h2 className="text-lg font-bold">{product.title}</h2>
+                                    <p className="text-sm mb-1"><strong className="text-orange-700 text-base">SKU : </strong> {product.sku}</p>
+                                    <p className="text-sm mb-1"><strong className="text-orange-700 text-base">Size :</strong> {product.size}</p>
+                                    <p className="text-sm mb-1"><strong className="text-orange-700 text-base">Quantity :</strong> {product.quantity}</p>
+                                    <p className="text-sm mb-1"><strong className="text-orange-700 text-base">Total Price : </strong>{orderDetails.totalPrice ? orderDetails.totalPrice :  'NAN'}</p>
+                                    <p className="text-sm mb-1"><strong className="text-orange-700 text-base">Order Date :</strong> {new Date(orderDetails.createdAt).toLocaleDateString()}</p>
+                                    <p className="text-sm mb-1"><strong className="text-orange-700 text-base">Status :</strong> {orderDetails.status}</p>
+
+                                </div>
+                            </CardContent>
+                            <CardFooter className="flex justify-center">
+                                <p className="text-sm"><strong className="text-orange-700 text-base">Order ID :</strong> {orderDetails.id}</p>
+                            </CardFooter>
+                        </Card>
+                    ))}
                 </div>
-                <Separator orientation="vertical" className="mx-2 h-10 w-px" />
-                <div className="grid flex-1 auto-rows-min gap-0.5">
-                  <div className="text-xs text-muted-foreground">Exercise</div>
-                  <div className="flex items-baseline gap-1 text-2xl font-bold tabular-nums leading-none">
-                    73
-                    <span className="text-sm font-normal text-muted-foreground">
-                      min
-                    </span>
-                  </div>
-                </div>
-                <Separator orientation="vertical" className="mx-2 h-10 w-px" />
-                <div className="grid flex-1 auto-rows-min gap-0.5">
-                  <div className="text-xs text-muted-foreground">Stand</div>
-                  <div className="flex items-baseline gap-1 text-2xl font-bold tabular-nums leading-none">
-                    14
-                    <span className="text-sm font-normal text-muted-foreground">
-                      hr
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </CardFooter>
-          </Card>
-        </div>
-      </div>
-    </>
-  );
+            </div>
+        </>
+    );
 }
 
 export default OrderDetails;
+
+
