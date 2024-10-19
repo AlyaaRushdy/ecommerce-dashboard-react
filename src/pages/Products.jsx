f4f86cdcb9d35a6602e77de4c3c99f912a8e7469
MostafaMagdy21 <mostafamagdy2196@gmail.com>
2024-10-19 14:02:33 +0300

    handel product

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,167 +1,40 @@
-import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-  CardDescription,
-} from "@/components/ui/card";
-import {
-  ChartContainer,
-  ChartTooltip,
-  ChartTooltipContent,
-} from "@/components/ui/chart";
 import Header from "../components/shared/Header";
-import { BellRing } from "lucide-react";
 import DataTable from "@/components/shared/DataTable";
 
 import { useEffect, useState } from "react";
 import axios from "axios";
 import productsColumns from "@/data table columns/productsColumns";
-
 export default function CardWithForm() {
-  const [products, setProducts] = useState([]);
+	const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/products/")
-      .then((res) => res.data)
-      .then((res) => {
-        setProducts(res.products);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+	const getProductsFromApi = async () => {
+		try {
+			const getProducts = await axios.get("http://localhost:5000/products/");
+			setProducts(getProducts.data.products);
+		} catch (err) {
+			console.log(err.message);
+		}
+	};
 
-  const notifications = [
-    {
-      title: "Your call has been confirmed.",
-      description: "1 hour ago",
-    },
-    {
-      title: "You have a new message!",
-      description: "1 hour ago",
-    },
-    {
-      title: "Your subscription is expiring soon!",
-      description: "2 hours ago",
-    },
-  ];
+	useEffect(() => {
+		getProductsFromApi();
+	}, []);
 
-  const chartData = [
-    { month: "January", desktop: 186 },
-    { month: "February", desktop: 305 },
-    { month: "March", desktop: 237 },
-    { month: "April", desktop: 73 },
-    { month: "May", desktop: 209 },
-    { month: "June", desktop: 214 },
-  ];
+	return (
+		<main className="p-5">
+			<Header currentPage={"Producst"} />
 
-  const chartConfig = {
-    desktop: {
-      label: "Desktop",
-      color: "hsl(var(--chart-1))",
-    },
-  };
-  return (
-    <main className="p-5">
-      {console.log(products)}
-      <Header currentPage={"Producst"} />
+			{/* start table */}
 
-      {/* start card */}
-      <section className="row py-4 grid grid-cols-12 gap-4">
-        <div className="card md:col-span-4 lg:col-span-4 col-span-12 ">
-          <Card>
-            <CardHeader>
-              <CardTitle>Notifications</CardTitle>
-              <CardDescription>You have 3 unread messages.</CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-4">
-              <div className=" flex items-center space-x-4 rounded-md border p-4">
-                <BellRing />
-                <div className="flex-1 space-y-1">
-                  <p className="text-sm font-medium leading-none">
-                    Push Notifications
-                  </p>
-                  <p className="text-sm text-muted-foreground">
-                    Send notifications to device.
-                  </p>
-                </div>
-              </div>
-              <div>
-                {notifications.map((notification, index) => (
-                  <div
-                    key={index}
-                    className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
-                  >
-                    <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-                    <div className="space-y-1">
-                      <p className="text-sm font-medium leading-none">
-                        {notification.title}
-                      </p>
-                      <p className="text-sm text-muted-foreground">
-                        {notification.description}
-                      </p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </CardContent>
-          </Card>
-        </div>
-        {/* start chart */}
-        <section className="chart md:col-span-8 lg:col-span-8 col-span-12">
-          <Card>
-            <CardHeader>
-              <CardTitle>Line Chart</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <ChartContainer config={chartConfig}>
-                <LineChart
-                  accessibilityLayer
-                  data={chartData}
-                  margin={{
-                    left: 12,
-                    right: 12,
-                  }}
-                >
-                  <CartesianGrid vertical={false} />
-                  <XAxis
-                    dataKey="month"
-                    tickLine={false}
-                    axisLine={false}
-                    tickMargin={8}
-                    tickFormatter={(value) => value.slice(0, 3)}
-                  />
-                  <ChartTooltip
-                    cursor={false}
-                    content={<ChartTooltipContent hideLabel />}
-                  />
-                  <Line
-                    dataKey="desktop"
-                    type="natural"
-                    stroke="var(--color-desktop)"
-                    strokeWidth={2}
-                    dot={false}
-                  />
-                </LineChart>
-              </ChartContainer>
-            </CardContent>
-          </Card>
-        </section>
-      </section>
-      {/* start table */}
-
-      <div>
-        <DataTable
-          columns={productsColumns()}
-          data={products}
-          tableTitle={"All Products"}
-          ButtonLink={"/products/addProduct"}
-          ButtonText={"Add Product"}
-        />
-      </div>
-    </main>
-  );
+			<div>
+				<DataTable
+					columns={productsColumns()}
+					data={products}
+					tableTitle={"All Products"}
+					ButtonLink={"/products/addProduct"}
+					ButtonText={"Add Product"}
+				/>
+			</div>
+		</main>
+	);
 }
